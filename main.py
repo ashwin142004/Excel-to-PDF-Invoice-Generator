6ea83be32de4e1f17953bc83cf658b779555aa8f
ashwin142004 <nmashwin145@gmail.com>
2025-01-28 18:34:35 +0530

    Final commit

--- main.py
+++ main.py
@@ -20,12 +20,38 @@ for filepath in filepaths:
     pdf.cell(50, 8, f'Date {date}', ln = 1)
 
     df = pd.read_excel(filepath, sheet_name='Sheet 1')
+
+    columns = df.columns
+    columns = [item.replace('_', ' ').title() for item in columns]
+    pdf.set_font('Times', 'B', 12)
+
+    pdf.cell(25, 8, f'{columns[0]}' , border=1)
+    pdf.cell(50, 8, f'{columns[1]}', border=1)
+    pdf.cell(40, 8, f'{columns[2]}', border=1)
+    pdf.cell(30, 8, f'{columns[3]}', border=1)
+    pdf.cell(30, 8, f'{columns[4]}', ln = 1, border=1)
+
     for index, row in df.iterrows():
         pdf.set_font('Times', '', 12)
-        pdf.cell(30, 8, f'{row["product_id"]}' , border=1)
-        pdf.cell(70, 8, f'{row["product_name"]}', border=1)
-        pdf.cell(30, 8, f'{row["amount_purchased"]}', border=1)
+        pdf.cell(25, 8, f'{row["product_id"]}' , border=1)
+        pdf.cell(50, 8, f'{row["product_name"]}', border=1)
+        pdf.cell(40, 8, f'{row["amount_purchased"]}', border=1)
         pdf.cell(30, 8, f'{row["price_per_unit"]}', border=1)
         pdf.cell(30, 8, f'{row["total_price"]}', ln = 1, border=1)
     
+    total_sum = df['total_price'].sum()
+    pdf.set_font('Times', '', 12)
+    pdf.cell(25, 8, "" , border=1)
+    pdf.cell(50, 8, "", border=1)
+    pdf.cell(40, 8, "", border=1)
+    pdf.cell(30, 8, "", border=1)
+    pdf.cell(30, 8, str(total_sum), ln = 1, border=1)
+
+    pdf.set_font('Times', 'B', 10)
+    pdf.cell(30, 8, f"The total price is {total_sum}", ln = 1)
+
+    pdf.set_font('Times', 'B', 10)
+    pdf.cell(30, 8, f"Thank you for your purchase", ln = 1)
+
+
     pdf.output(f'PDFs/{filename}.pdf')
